5a3ddbc2b545b6bb1521a7f794d4c2339191d92f
Prana Alfath Rais <dafaaje222@gmail.com>
2025-10-07 22:22:01 +0700

    Ini progres dari saya menambahkan bagian library python

---
+++ NaiveBayes.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import time
+import re
+from wordcloud import WordCloud
+#%matplotlib inline
+import plotly.express as px
+import warnings
+import sklearn.metrics as sm
+import imblearn
+import string
+import nltk
+import re
+
+#fitur_nltk
+from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
+from nltk.corpus import stopwords
+from nltk import FreqDist
+
+
+
+warnings.filterwarnings('ignore')
